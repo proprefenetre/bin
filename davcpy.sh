5a6bc612c66abc55dce48d692d3eba5f92fb8617
proprefenetre <proprefenetre@gmail.com>
2016-12-06 09:04:15 +0100

    stuff to do with rowing clips

---
+++ davcpy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# from: http://superuser.com/questions/523194/how-to-recursively-upload-a-directory-to-a-webdav-server-through-https-from-the
+
+usage () { echo "$0 <src> <cadaver-args>*" >/dev/stderr; }
+error () { echo "$1" >/dev/stderr; usage; exit 1; }
+
+test $# '<' 3 || \
+    error "Source and cadaver arguments expected!";
+
+src="$1"; shift;
+test -r "$src" || \
+    error "Source argument should be a readable file or directory!";
+
+cd "$(dirname "$src")";
+src="$(basename "$src")";
+root="$(pwd)";
+rc="$(mktemp)";
+{
+    find "$src" '(' -type d -a -readable ')' \
+    -printf 'mkcol "%p"\n';
+    find "$src" '(' -type f -a -readable ')' \
+    -printf 'cd "%h"\nlcd "%h"\n'            \
+    -printf 'mput "%f"\n'                    \
+    -printf 'cd -\nlcd "'"$root"'"\n';
+    echo "quit";
+} > "$rc";
+
+cadaver -r "$rc" "$@";
+rm -f "$rc";
