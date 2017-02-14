6a4f4119b2d5cfd83a232ba5e3e79640f43d23a3
proprefenetre <proprefenetre@gmail.com>
2017-02-14 13:27:58 +0100

    change backup target, rename 'workspace'

--- backup.sh
+++ backup.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-
-# strict mode:
-set -e
-set -u
-set -o pipefail
-
 if [[ $# -eq 0 ]]; then
     echo "No arguments supplied" 
     exit 1
 fi
 
-TARGET=$(find /media -name "*usb-Seagate_Expansio")
+# TARGET=$(find /media -name "*usb-Seagate_Expansio")
+TARGET=~/usb
 
-include=~/.rsync-include-list
-exclude=~/.rsync-exclude-list
+include=./rsync-include-list
+exclude=./rsync-exclude-list
 
 case $1 in
     -t ) echo $include > bla.txt ;;
-    -d ) sudo rsync --dry-run -aAXuv --info=progress --include-from=$include --exclude-from=$exclude /* /media/sdb1-usb-Seagate_Expansio/new_backup 2>&1 | tee -a /home/niels/files/rsync-dry-$(date +%d%m%Y%H%M%S) 
+    -d ) echo "Dry run" && sudo rsync --dry-run -aAXuv --info=progress --include-from=$include --exclude-from=$exclude /* /media/sdb1-usb-Seagate_Expansio/new_backup 2>&1 | tee -a /home/niels/files/rsync-dry-$(date +%d%m%Y%H%M%S) 
     ;;
 
     -r ) sudo rsync -aAXuvS --info=progress --include-from=$include --exclude-from=$exclude /* $TARGET/arch1 2>&1 | tee -a /home/niels/files/rsync-all-$(date +%d%m%Y%H%M%S)
