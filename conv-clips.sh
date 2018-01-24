8e5241915a33c456fa5ab0766adbbb141b62a5c4
proprefenetre <proprefenetre@gmail.com>
2018-01-24 14:36:37 +0100

    proper name

--- conv-clips.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-# IFS=$(echo -en "\n\b");
-# for i in *.MTS; do
-[[ $# -lt 1 ]] && echo "gimme a file" && exit 1
-
-fname=$(basename "$1" .MTS)
-
-ffmpeg -i "$1" -an "$fname.mp4"
