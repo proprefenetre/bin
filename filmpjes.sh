fb6b35d023e6d05e951211442d0f4fdced9d936c
proprefenetre <proprefenetre@gmail.com>
2019-05-29 15:52:00 +0200

    remove cruft

--- filmpjes.sh
+++ filmpjes.sh
@@ -1,14 +1,13 @@
 #! /usr/bin/env bash
-# set -e -u
 
 usage () { echo "$(basename $0) <src> <cadaver-args>" >/dev/stderr; }
-error () { echo "$1" >/dev/stderr; usage; exit 1; }
-
 # dav="https://proprefenetre.stackstorage.com/remote.php/webdav/"
 
 # [[ $# < 3 ]] || error "Source and cadaver arguments are required!"
 
-src="$1"; shift;    # $1 is now target
+src="$1"
+shift 
+
 [[ -r "$src" ]] || error "Source should be readable"
 
 cd "$(dirname $src)"
