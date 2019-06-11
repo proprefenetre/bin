931d52483e2e5f82820786546e713ddc1c2ddb48
proprefenetre <proprefenetre@gmail.com>
2019-06-11 20:34:23 +0200

    update camera-scripts

--- filmpjes.sh
+++ filmpjes.sh
@@ -1,12 +1,9 @@
 #! /usr/bin/env bash
 
-usage () { echo "$(basename $0) <src> <cadaver-args>" >/dev/stderr; }
-# dav="https://proprefenetre.stackstorage.com/remote.php/webdav/"
-
-# [[ $# < 3 ]] || error "Source and cadaver arguments are required!"
+usage () { echo "$(basename $0) <src> <dest>" >/dev/stderr; }
 
 src="$1"
-shift 
+dest="$2"
 
 [[ -r "$src" ]] || error "Source should be readable"
 
@@ -15,9 +12,8 @@ src="$(basename $src)"
 
 # convert .MTS to .mp4 (without sound)
 IFS=$(echo -en "\n\b");
-for i in $src/*.MTS; do
+for i in "$src"/*.MTS; do
     fname="$(date --date="$src" "+%a-%d-%m-%Y")_$(basename "$i" .MTS).mp4"
-    ffmpeg -i "$i" -an "$src/$fname"
-    rm "$i"
+    ffmpeg -i "$i" -an "$dest/$fname"
 done
 
